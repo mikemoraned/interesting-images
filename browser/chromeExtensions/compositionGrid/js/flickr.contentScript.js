adbc19df9f24174c25bf7aafaafcdc008d3a3565
mikemoraned <mike@houseofmoran.com>
2012-04-07 23:35:52 +0100

    got an svg version of overlays working
    they are the same, except there is a slight difference in how they overlay.
    also, the svg version gets translated differently than the canvas, when the browser is zoomed.

--- browser/chromeExtensions/compositionGrid/js/flickr.contentScript.js
+++ browser/chromeExtensions/compositionGrid/js/flickr.contentScript.js
@@ -2,6 +2,33 @@ console.log("flickr content script loaded")
 
 var img;
 
+function svgLines(paper, lines, stroke, name) {
+    for (var i = 0; i < lines.length; i++) {
+	var line = lines[i];
+	var start = line[0];
+	var end = line[1];
+	var p = paper.path("M" + start[0] + "," + start[1] + "L" + end[0] + "," + end[1]).attr({"stroke": stroke});
+	p.node.setAttribute("class", name);
+	console.dir(p);
+    }
+}
+
+function svgGrid(paper) {
+    var width = paper.width;
+    var height = paper.height;
+    var diagonalMethod = [[[0.0,   0.0],    [Math.min(width, height),         Math.min(width, height)]],
+			  [[0.0,   height], [Math.min(width, height),         height - Math.min(width, height)]],
+			  [[width, 0.0],    [width - Math.min(width, height), Math.min(width, height)]],
+			  [[width, height], [width - Math.min(width, height), height - Math.min(width, height)]],
+			 ];
+    svgLines(paper, diagonalMethod, "#f00", "diagonal-method");
+    var ruleOfThirds = [[[0.0,               height / 3.0],       [width,             height / 3.0]],
+			[[0.0,               2.0 * height / 3.0], [width,             2.0 * height / 3.0]],
+			[[width / 3.0,       0.0],                [width / 3.0,       height]],
+			[[2.0 * width / 3.0, 0.0],                [2.0 * width / 3.0, height]]];
+    svgLines(paper, ruleOfThirds, "#fff", "rule-of-thirds");
+}
+
 function draw() {
     var canvas = document.getElementById("canvas");
     if (img && canvas) {
@@ -57,11 +84,13 @@ $("div.photo-div img").each(function (i) {
     console.log("Created Image");
     var imgElem = $(this);
     var parentDiv = $(this).parent();
+    var paper = Raphael($(this).offset().left, $(this).offset().top, $(this).width(), $(this).height());
+    svgGrid(paper);
     img.onload = function() {
 	parentDiv.append("<canvas id='canvas' width=\"" + img.width + "\" height=\"" + img.height + "\" />");
 	console.log("Created canvas");
-	imgElem.css("display","none");
-	console.log("Hid original image");
+	//imgElem.css("display","none");
+	//console.log("Hid original image");
 	draw();
     }
 });
